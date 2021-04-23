32a1f97d791debd584d38e5209ead242363c2778
Christopher Schütz <schuetzchristopher@icloud.com>
2021-04-23 18:35:45 +0200

    initial commit

---
+++ healthkit-starter/Shared/healthkit_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthkit_starterApp.swift
+//  Shared
+//
+//  Created by Christopher Schütz on 23.04.21.
+//
+
+import SwiftUI
+
+@main
+struct healthkit_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
