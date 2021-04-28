1437ee45f3b155ef3544e3f214fab702f902a1e0
Christopher Schütz <schuetzchristopher@icloud.com>
2021-04-28 10:11:27 +0200

    make state var private + add comments on state property wrappers

--- healthkit-starter/Shared/ContentView.swift
+++ healthkit-starter/Shared/ContentView.swift
@@ -43,11 +43,28 @@ class Model: ObservableObject {
 }
 
 struct ContentView: View {
+    // State vs Observed Object vs EnvironmentObject:
+    // https://www.hackingwithswift.com/quick-start/swiftui/whats-the-difference-between-observedobject-state-and-environmentobject#:~:text=The%20rule%20is%20this%3A%20whichever,don't%20own%20it%20directly.
     
-    @State var healthKitManager: HealthKitManager? = nil
+    @State private var healthKitManager: HealthKitManager? = nil
     
+    // For more complex properties - when you have a custom type you want to
+    // use that might have multiple properties and methods, or might be shared
+    // across multiple views - you will often use Observed Object instead.
+    //
+    // The devloper is responsible for managing the data herself. She needs to
+    // create an instance of the class, create its own properties, and so on.
+    //
+    // There are several ways for an observed object to notify views that important
+    // data has changed, but the easiest is using the @Published property wrapper.
     @ObservedObject var model: Model = Model(workouts: [])
     
+    // State is great for simple properties that belong to a specific view and
+    // never get used outside that view, so as a result it's important to mark those
+    // properties as being private to re-enforce the idea that such state is specifically
+    // designed never to escape its view.
+    //
+    // SwiftUI manages the memory requirements for this property.
     @State private var objectLoaded = false
     
     func getResults() {
@@ -108,14 +125,14 @@ func makeDate(year: Int, month: Int, day: Int, hr: Int, min: Int, sec: Int) -> D
 }
 
 struct ContentView_Previews: PreviewProvider {
-    @StateObject private static var model: Model = Model(
-        workouts: [
-//            HKWorkout(
-//                activityType: .americanFootball,
-//                start: makeDate(year: 2021, month: 4, day: 26, hr: 12, min: 0, sec: 0),
-//                end: makeDate(year: 2021, month: 4, day: 26, hr: 13, min: 0, sec: 0)
-//            )
-        ])
+    // StateObject is a specialized version of ObservedObject and it works in almost
+    // exactly the same way.
+    // The important difference is ownership - which view created the object, and which
+    // view is just watching it. Whichever view is the first to create the object must
+    // use StateObject, to tell SwiftUI it is the owner of the data and is responsible for keeping it alive.
+    // All other view must use @ObservedObject, to tell SwiftUI they want to watch the object for changes
+    // but don't own it directly.
+    @StateObject private static var model: Model = Model(workouts: [])
     
     static var previews: some View {
         ContentView(
