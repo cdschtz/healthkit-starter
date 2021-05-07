c85f6d21b0d885380dc0cfd0d06bc3684b375ece
Christopher Schütz <schuetzchristopher@icloud.com>
2021-05-07 10:25:08 +0200

    add pace graph conversion

--- analytics/visuals.py
+++ analytics/visuals.py
@@ -6,6 +6,25 @@ from utils.utils import process_workout, get_speed_chart_values, get_heart_rate_
     get_location_altitude_chart_values
 
 
+def meter_per_second_to_min_per_km(x: float):
+    # e.g. 0.18 km / 1 min
+    km_per_min = (x / 1000.0) * 60.0
+    # we want: 1 km / x min?
+    min_per_km = 1.0 / km_per_min
+
+    return min_per_km
+
+
+def visualize_speed_over_time(target_path: Path, graph_values: Tuple[List[datetime], List[float]]) -> None:
+    x_values, y_values = graph_values
+
+    y_values = [meter_per_second_to_min_per_km(value) for value in y_values]
+
+    plt.plot(x_values, y_values)
+    plt.savefig(target_path)
+    plt.close()
+
+
 def visualize_graph_values(target_path: Path, graph_values: Tuple[List[datetime], List[float]]) -> None:
     x_values, y_values = graph_values
 
@@ -21,6 +40,6 @@ if __name__ == '__main__':
     altitude_values = get_location_altitude_chart_values(extracted_workout)
     speed_values = get_speed_chart_values(extracted_workout)
 
-    visualize_graph_values(Path("data/20210504-WorkoutApple/heart_rate.png"), heart_rate_values)
-    visualize_graph_values(Path("data/20210504-WorkoutApple/altitude.png"), altitude_values)
-    visualize_graph_values(Path("data/20210504-WorkoutApple/speed.png"), speed_values)
+    visualize_graph_values(Path("data/20210504-WorkoutApple/assets/heart_rate.png"), heart_rate_values)
+    visualize_graph_values(Path("data/20210504-WorkoutApple/assets/altitude.png"), altitude_values)
+    visualize_speed_over_time(Path("data/20210504-WorkoutApple/assets/speed.png"), speed_values)
